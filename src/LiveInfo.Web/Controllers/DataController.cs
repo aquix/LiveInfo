757e4451369e0fdd0af6eb9b650b1290ea792055
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-10-21 13:54:57 +0300

    Implement simple table

--- src/LiveInfo.Web/Controllers/DataController.cs
+++ src/LiveInfo.Web/Controllers/DataController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LiveInfo.Web.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +12,41 @@ namespace LiveInfo.Web.Controllers
     [Route("api/[controller]")]
     public class DataController : Controller
     {
+        private IList<Item> data;
+
+        public DataController() {
+            data = new List<Item> {
+                new Item {
+                    Id = 1,
+                    Name = "Something",
+                    Rating = 1
+                },
+                new Item {
+                    Id = 2,
+                    Name = "Something1",
+                    Rating = 5
+                },
+                new Item {
+                    Id = 3,
+                    Name = "Something231",
+                    Rating = 4
+                },
+                new Item {
+                    Id = 4,
+                    Name = "Something32132",
+                    Rating = 3
+                },
+                new Item {
+                    Id = 5,
+                    Name = "Something321",
+                    Rating = 2
+                },
+            };
+        }
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
+        public IEnumerable<Item> Get() {
+            return data;
         }
     }
 }
--- src/LiveInfo.Web/wwwroot/js/site.js
+++ src/LiveInfo.Web/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿// Write your Javascript code.
+﻿const API_PATH = '/api/data';
+const REQUEST_INTERVAL = 1000;
+
+class AppViewModel {
+    constructor() {
+        this.data = ko.observable([]);
+    }
+}
+
+class ItemViewModel {
+
+}
+
+let appViewModel = new AppViewModel();
+ko.applyBindings(appViewModel);
+
+let requestInterval = setInterval(() => {``
+    fetch(API_PATH).then(res => {
+        return res.json();
+    }).then(data => {
+        appViewModel.data(data);
+    })
+}, REQUEST_INTERVAL);
